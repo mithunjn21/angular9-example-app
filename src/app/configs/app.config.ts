275e8b898c4111cc248a287c5232d838fb5e8553
mithun joseph <mithunjn21@gmail.com>
2021-03-01 20:53:37 +0530

    repository URL updated

--- src/app/configs/app.config.ts
+++ src/app/configs/app.config.ts
@@ -6,7 +6,7 @@ export const AppConfig: any = {
   votesLimit: 3,
   topHeroesLimit: 5,
   snackBarDuration: 3000,
-  repositoryURL: 'https://github.com/ismaestro/angular8-example-app',
+  repositoryURL: 'https://github.com/mithunjn21/angular9-example-app',
   sentryDSN: 'https://38434a1b115f41d3a31e356cdc496c06@sentry.io/1315526',
   cspDirectives: {
     defaultSrc: [
